d1b8ac123928ffec2ae82548288345a654256927
sanchez <dfitz360@gmail.com>
2020-04-26 12:59:57 +1000

    its working and under good conditions

--- Sanchez.OOS.Client.Window/Program.fs
+++ Sanchez.OOS.Client.Window/Program.fs
@@ -1,16 +1,23 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Diagnostics
 open System.Threading
+open Sanchez.Data
 open Sanchez.Game.Core
 open Sanchez.Game.Platformer
 open Sanchez.OOS.Client.Window
 open Sanchez.OOS.Client.Window.Assets
 open Sanchez.OOS.Client.Window.Characters
+open Sanchez.OOS.Core
+open Sanchez.OOS.Server
+open Sanchez.Socketier
+open FSharp.Data.UnitSystems.SI.UnitNames
 
 [<EntryPoint>]
 let main argv =
     let serverPort = 25599
+    let host = "127.0.0.1"
     printf "Enter Player Name: "
     let name = Console.ReadLine()
     
@@ -23,6 +30,39 @@ let main argv =
     let cToken = new CancellationToken()
     
     MainPlayer.loadMainPlayer manager name
+    
+    let connectToServer() = Client.connectToServer ServerAction.decode ClientAction.encode host serverPort cToken
+    let createServerAndConnect () =
+        ServerManager.createServer serverPort cToken
+        |> AsyncResult.bind connectToServer
+    let (poster, actioner) =
+        connectToServer()
+        |> AsyncResult.bindError (fun _ -> createServerAndConnect())
+        |> AsyncResult.synchronously
+        
+    let mutable pingGuids: Map<Guid, Stopwatch> = Map.empty
+    manager.AddSchedule 0.5<second> (fun () ->
+        let id = Guid.NewGuid()
+        let stW = new Stopwatch()
+        
+        pingGuids <- pingGuids |> Map.add id stW
+        stW.Start()
+        id |> Ping |> poster
+        
+        true)
+    actioner.AddActioner "pingpong" (fun (ip, a) ->
+        match a with
+        | Pong id ->
+            let stW = Map.find id pingGuids
+            stW.Stop()
+            
+            let elapsed =
+                stW.ElapsedTicks
+                |> float
+                |> (fun x -> x / (Stopwatch.Frequency |> float))
+                |> ((*) 1.<second>)
+            printfn "Received a ping of %fms" elapsed
+            Some ())
     
     manager.Run()
     
---
+++ Sanchez.OOS.Server/ServerManager.fs
@@ -0,0 +1,24 @@
+﻿module Sanchez.OOS.Server.ServerManager
+
+open Sanchez.Data
+open Sanchez.OOS.Core
+open Sanchez.Socketier
+open Sanchez.Socketier.Server
+
+let createServer port cToken =
+    asyncResult {
+        let! (poster, actioner) = Server.createServer ClientAction.decode ServerAction.encode port cToken
+        
+        do! async {
+            do! Async.Sleep 500
+            return Ok ()
+        }
+        
+        do actioner.AddActioner "pingpong" (fun (ip, a) ->
+            match a with
+            | Ping id ->
+                poster (SenderAddress.SingleSend ip, Pong id)
+                Some ())
+        
+        return ()
+    }
