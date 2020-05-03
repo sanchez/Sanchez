f75010952a4ced0b92c06286a2c5076b0a02a5a3
sanchez <dfitz360@gmail.com>
2020-05-03 11:49:03 +1000

    Started physics, parking for now

---
+++ Sanchez.Game.Core/BoundingBox.fs
@@ -0,0 +1,19 @@
+﻿namespace Sanchez.Game.Core
+
+type BoundingBox =
+    {
+        Min: Position
+        Max: Position
+    }
+
+module BoundingBox =
+    let create min max =
+        {
+            BoundingBox.Min = min
+            Max = max
+        }
+        
+    let isIntersecting (a: BoundingBox) (b: BoundingBox) =
+        if (a.Max.X < b.Min.X || a.Min.X > b.Max.X) then false
+        elif (a.Max.Y < b.Min.Y || a.Min.Y > b.Max.Y) then false
+        else true
